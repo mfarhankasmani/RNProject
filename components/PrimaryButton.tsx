298b58b62fa7ffe6331a62e37e15d570ceb1fb1b
farhan <farhankasmani@live.com>
2022-10-17 23:17:44 +1100

    button styling

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,15 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet, GestureResponderEvent } from "react-native";
 
 interface PrimaryButtonProp {
   children: React.ReactNode;
 }
 
 const PrimaryButton: React.FC<PrimaryButtonProp> = ({ children }) => {
+  const pressHandler = (event: GestureResponderEvent) => {
+    console.log("pressing button", { event });
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={style.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) =>
+          pressed
+            ? [style.pressed, style.buttonInnerContainer]
+            : style.buttonInnerContainer
+        }
+      >
+        <Text style={style.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const style = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden"
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center"
+  },
+  pressed: {
+    opacity: 0.75
+  }
+});
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -18,16 +24,27 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     padding: 16,
     marginTop: 100,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4, // applies shadow only on android
     //for ios below style props are used
     shadowColor: "black",
     shadowOffset: {
       width: 1,
-      height: 1,
+      height: 1
     },
     shadowRadius: 6,
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.25
   },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
